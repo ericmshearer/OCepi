1de123cf1d617d86f5d4ddfba2b3486e5245cf0c
Eric Shearer <70504145+ericmshearer@users.noreply.github.com>
2023-12-14 12:42:31 -0800

    Update assign_season.R
    Fix season assignment

--- R/assign_season.R
+++ R/assign_season.R
@@ -1,3 +1,21 @@
+#' Retain class with ifelse inside a function
+#'
+#' @param cond Condition to test
+#' @param yes Returned output if true
+#' @param no Returned output if false
+#'
+#' @return Tested condition while retaining original class.
+#'
+#' @examples
+#' test_date = as.Date("2023-01-01)
+#' safe.ifelse(test_date < as.Date("2023-01-02), test_date, test_date - 5)
+safe.ifelse <- function(cond, yes, no){
+  class.y <- class(yes)
+  X <- ifelse(cond, yes, no)
+  class(X) <- class.y;
+  return(X)
+}
+
 #' Assign Respiratory Season
 #'
 #' @param date Input date.
@@ -14,14 +32,23 @@ assign_season <- function(date){
     stop("Input not in date format.")
   }
 
-  year = to_mmwr_date(date, type = "year")
-  week = to_mmwr_date(date, type = "week")
+  init_week_num = OCepi::to_mmwr_date(OCepi::week_ending_date(date), "week")
 
-  if(week %in% c(40:53)){
-    season = paste(year, substr(year+1,3,4), sep = "-")
-  } else {
-    season = paste(year-1, substr(year,3,4), sep = "-")
-  }
+  adjust = ifelse(init_week_num %in% c(40:53), init_week_num - 40, 40 - init_week_num) * 7
 
-  return(season)
+  # init_season_start = OCepi::week_ending_date(date + adjust) - 6
+  init_season_start = safe.ifelse(
+    init_week_num %in% c(40:53),
+    OCepi::week_ending_date(date - adjust) - 6,
+    OCepi::week_ending_date(date + adjust) - 6
+  )
+
+  season_start = safe.ifelse(date < init_season_start, init_season_start - 364, init_season_start)
+
+  season_end = season_start + 363
+
+  year1 = as.numeric(strftime(season_start, "%Y"))
+  year2 = as.numeric(strftime(season_end, "%Y"))
+
+  return(paste(year1,substr(year2,3,4), sep = "-"))
 }
