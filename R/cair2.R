1ab615c2c626e168952b1b5038a38f6dec54d81f
Eric Shearer <ericshearer@me.com>
2024-03-13 18:43:14 -0700

    Updated vignette, modified hcv age group, added cair2 peds age group

--- R/cair2.R
+++ R/cair2.R
@@ -11,7 +11,7 @@
 #' baby_name("BABYBOY")
 #' baby_name("Twin Girl")
 baby_name <- function(name){
-  combined_pattern <- "(?i)(.*baby.*|bab(\\s)?boy|babyboy|mom|infant|bab|\\bmother\\b|\\bmomma\\b|\\bmama\\b|babygirl|girl|boy|twin|baby girl|a twin|boy a|girl a|boy b|girl b)"
+  combined_pattern <- "(?i)(.*baby.*|bab(\\s)?boy|babyboy|mom|infant|bab|\\bmother\\b|\\bmomma\\b|\\bmama\\b|babygirl|girl|boy|twin|baby girl|a twin|boy a|girl a|boy b|girl b|NEWBORN)"
   name = ifelse(grepl(combined_pattern, name, perl = TRUE), NA, name)
   return(name)
 }
