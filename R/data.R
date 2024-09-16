18a568e2aac2db4bd40f1e4ddeee3bcb20ebf960
Eric Shearer <ericshearer@me.com>
2024-09-16 15:32:11 -0700

    Add oc_city_sf

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' OC Zip Code Basemap
+#'
+#' @docType data
+#'
+#' @usage data(oc_zip_sf)
+#'
+#' @format An object of class sf.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(oc_zip_sf)
+"oc_zip_sf"
+
+#' OC City Basemap
+#'
+#' @docType data
+#'
+#' @usage data(oc_city_sf)
+#'
+#' @format An object of class sf.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(oc_city_sf)
+"oc_city_sf"
